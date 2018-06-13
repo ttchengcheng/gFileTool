c8f498ec188adec7e06152fbdb3a3f62bd1c15bd
Tao <tchengim30@sina.com>
2018-06-13 18:58:41 +0800

    improve output | support command line parameters

--- fsync/main/main.go
+++ fsync/main/main.go
@@ -20,13 +20,8 @@ import (
 
 type taskFn func() error
 
-func group(name string) {
-	fmt.Printf("\n%s\n\n", name)
-}
-
 func task(fn taskFn, format string, a ...interface{}) {
-	const indent = "-"
-	fmt.Printf(indent+format, a...)
+	fmt.Printf(format+"...", a...)
 	if err := fn(); err != nil {
 		fmt.Println("failed.")
 		panic(err)
@@ -38,21 +33,30 @@ func task(fn taskFn, format string, a ...interface{}) {
 func checkFolder(src fsync.Filer, des fsync.Filer) {
 	// source folder should exist and be a folder
 	srcInfoRoot, err := os.Stat(src.Dir())
-	if err != nil {
-		panic(err)
-	}
-	if !srcInfoRoot.IsDir() {
-		panic(errors.New("source path is not a valid folder"))
-	}
+	task(func() error {
+		if err != nil {
+			return err
+		}
+		if !srcInfoRoot.IsDir() {
+			return errors.New("source path is not a valid folder")
+		}
+		return nil
+	}, "check source path")
 
 	// destination folder should be a folder and exist
 	desInfoRoot, err := os.Stat(des.Dir())
 	if os.IsNotExist(err) {
-		des.Mkdir(des.Dir(), srcInfoRoot.Mode().Perm())
+		task(func() error {
+			return des.Mkdir(des.Dir(), srcInfoRoot.Mode().Perm())
+		}, "creating [%s]", des.Dir())
 	}
 	if !desInfoRoot.IsDir() {
-		des.Remove(des.Dir())
-		des.Mkdir(des.Dir(), srcInfoRoot.Mode().Perm())
+		task(func() error {
+			return des.Remove(des.Dir())
+		}, "removing [%s]", des.Dir())
+		task(func() error {
+			return des.Mkdir(des.Dir(), srcInfoRoot.Mode().Perm())
+		}, "creating [%s]", des.Dir())
 	}
 }
 
@@ -60,12 +64,12 @@ func readFileList(src fsync.Filer, des fsync.Filer) (listSource fsync.FileList, 
 	task(func() error {
 		listSource = make(fsync.FileList)
 		return src.GetList(&listSource)
-	}, "[%s]...", src.Dir())
+	}, "reading list [%s]", src.Dir())
 
 	task(func() error {
 		listDestination = make(fsync.FileList)
 		return des.GetList(&listDestination)
-	}, "[%s]...", des.Dir())
+	}, "reading list [%s]", des.Dir())
 
 	return
 }
@@ -76,26 +80,23 @@ func sync(src fsync.Filer, des fsync.Filer) {
 		if err := recover(); err != nil {
 			fmt.Println(err)
 		} else {
-			fmt.Println("\nALL DONE!")
+			fmt.Println()
+			fmt.Println("all done!")
 		}
 	}()
 
-	group("check source/destination folders")
 	checkFolder(src, des)
-
-	group("Get file list")
 	listSource, listDestination := readFileList(src, des)
 
-	group("Sync files")
 	indexes := cache.Indexer{}
 	indexPath := filepath.Join(des.Dir(), ".sync-cache")
 	task(func() error {
 		return indexes.Load(indexPath)
-	}, "loading indexes [%s]...", indexPath)
+	}, "loading indexes [%s]", indexPath)
 
 	defer task(func() error {
 		return indexes.Save()
-	}, "writing index file [%s]...", indexPath)
+	}, "writing indexes [%s]", indexPath)
 
 	removeDirs := make([]string, 0)
 	for path, srcInfo := range listSource {
@@ -106,18 +107,18 @@ func sync(src fsync.Filer, des fsync.Filer) {
 			if srcInfo.IsDir() {
 				task(func() error {
 					return des.Mkdir(desPath, srcInfo.Mode().Perm())
-				}, "creating [%s]...", desPath)
+				}, "creating [%s]", desPath)
 			} else {
 				task(func() error {
 					return des.Copy(srcPath, desPath, srcInfo.Mode().Perm())
-				}, "copying [%s]...", desPath)
+				}, "copying [%s]", desPath)
 			}
 		} else {
 			if desInfo.IsDir() {
 				if !srcInfo.IsDir() {
 					task(func() error {
 						return des.Remove(desPath)
-					}, "removing [%s]...", desPath)
+					}, "removing [%s]", desPath)
 
 					removeDirs = append(removeDirs, path+"/")
 				}
@@ -125,26 +126,50 @@ func sync(src fsync.Filer, des fsync.Filer) {
 				if indexes.Checksum(srcPath, srcInfo) != indexes.Checksum(desPath, desInfo) {
 					task(func() error {
 						return des.Copy(srcPath, desPath, srcInfo.Mode().Perm())
-					}, "copying [%s]...", desPath)
+					}, "copying [%s]", desPath)
 				}
 			}
 			delete(listDestination, path)
 		}
 	}
 
-	group("remove unmatched files")
 	for path := range listDestination {
 		desPath := filepath.Join(des.Dir(), path)
 
 		task(func() error {
 			return des.Remove(desPath)
-		}, "removing [%s]...", desPath)
+		}, "removing [%s]", desPath)
 	}
 }
 
 func main() {
-	srcDir := "/Users/hoolai/git/hScene"
-	desDir := "/Users/hoolai/Downloads/temp"
+	argc := len(os.Args)
+	if argc < 2 {
+		// TODO: help()
+		return
+	}
+
+	srcDir, err := filepath.Abs(os.Args[1])
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	desDir, err := filepath.Abs(".")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	if argc > 2 {
+		desDir, err = filepath.Abs(os.Args[2])
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+	}
+
+	// srcDir := "/Users/hoolai/git/hScene"
+	// desDir := "/Users/hoolai/Downloads/temp"
 	filter := &fsync.SkipSetting{}
 	filter.Parse(".git/\n.sync-cache")
 
